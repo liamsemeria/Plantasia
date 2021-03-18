8e83a34b71696d68b2f7a83ff21bf8f02e5fb686
Liam <liamsemeria@gmail.com>
2021-03-18 22:33:50 +0000

    added guide

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
     Interpreter p(argv[1]);
     p.lex();
     //p.printCode();
-    p.printCode();
+    //p.printCode();
     std::string s = "asd";
     //if (true && (s[-1]=='c')) std:: cout << "asdasd";
     p.interpret();
